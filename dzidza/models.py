0c2caa5ed561bcf9d57fbad3e0502379fd7623d0
ikhovryak <ikhovryak@haverford.edu>
2020-02-08 15:59:32 -0500

    Edited mentorship

--- dzidza/models.py
+++ dzidza/models.py
@@ -15,6 +15,7 @@ class User(db.Model, UserMixin):
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
+    # mentor = db.Column(db.String(60), nullable=True, default="No mentor yet")
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
